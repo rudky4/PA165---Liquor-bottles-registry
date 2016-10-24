7f0d23d3bf2465cbefe60662da25ad74da326864
Jakub Fiser <fiser33@seznam.cz>
2016-10-24 15:23:11 +0200

    Person entity created

---
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/PersonDAO.java
@@ -0,0 +1,27 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entity.Person;
+import cz.muni.fi.pa165.enums.PersonRole;
+
+import java.util.List;
+
+/**
+ * @author jakub
+ *         24/10/2016
+ */
+public interface PersonDAO {
+
+    void createPerson(Person person);
+
+    Person updatePerson(Person person);
+
+    void deletePerson(Person person);
+
+    Person findById(long id);
+
+    Person findByLogin(String login);
+
+    List<Person> findAll();
+
+    List<Person> getPersonsOfRole(PersonRole role);
+}
