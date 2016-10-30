0ac3d4bfa134de25e7c2f372cfc976f4c1cef759
mitko501 <422190@mail.muni.cz>
2016-10-30 13:04:49 +0100

    Add javadoc to bottle and bottleType DAO

--- persistence/src/main/java/cz/muni/fi/pa165/dao/BottleDAO.java
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/BottleDAO.java
@@ -8,14 +8,36 @@ import java.util.Collection;
  * @author mhajas
  */
 public interface BottleDAO {
+
+    /**
+     * Create bottle in database
+     * @param bottle object to create
+     */
     void createBottle(Bottle bottle);
 
+    /**
+     * Get all bottles from database
+     * @return Collection of bottles
+     */
     Collection<Bottle> getAllBottles();
 
+    /**
+     * Get bottle with ID
+     * @param id of bottle
+     * @return Bottle
+     */
     Bottle getBottleById(Long id);
 
+    /**
+     * Update new values for bottle
+     * @param bottle with new values
+     */
     void updateBottle(Bottle bottle);
 
+    /**
+     * Remove bottle
+     * @param bottle to remove
+     */
     void deleteBottle(Bottle bottle);
 }
 
--- persistence/src/main/java/cz/muni/fi/pa165/dao/BottleTypeDAO.java
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/BottleTypeDAO.java
@@ -8,14 +8,35 @@ import java.util.Collection;
  * @author mhajas
  */
 public interface BottleTypeDAO {
+    /**
+     * Create new bottle type
+      * @param bottleType new type to create
+     */
     void createBottleType(BottleType bottleType);
 
+    /**
+     * Get all bottle types from database
+     * @return Collection of all types
+     */
     Collection<BottleType> getAllBottleTypes();
 
+    /**
+     * Get bottle type by id
+     * @param id of bottle type to find
+     * @return BotttleType
+     */
     BottleType getBottleTypeById(Long id);
 
+    /**
+     * Update bottle type to new values
+     * @param bottleType object with new values
+     */
     void updateBottleType(BottleType bottleType);
 
+    /**
+     * Removes bottle type from database
+     * @param bottleType to delete
+     */
     void deleteBottleType(BottleType bottleType);
 }
 
