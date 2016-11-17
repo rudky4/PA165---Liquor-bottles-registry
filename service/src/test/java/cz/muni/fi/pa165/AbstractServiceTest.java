c9e29c64f7ae5e640b5204d5f21026ec91ae1065
Jakub Fiser <Fiser33@seznam.cz>
2016-11-17 14:39:18 +0100

    tests use only junit, testng removed

--- service/src/test/java/cz/muni/fi/pa165/AbstractServiceTest.java
+++ service/src/test/java/cz/muni/fi/pa165/AbstractServiceTest.java
@@ -2,20 +2,15 @@ package cz.muni.fi.pa165;
 
 import cz.muni.fi.pa165.configuration.ServiceConfiguration;
 
-import org.junit.BeforeClass;
-import org.mockito.MockitoAnnotations;
+import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author Jakub Fiser
  * @date 16/11/2016
  */
 @ContextConfiguration(classes = ServiceConfiguration.class)
-public class AbstractServiceTest extends AbstractTransactionalTestNGSpringContextTests {
-
-    @BeforeClass
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-    }
+@RunWith(SpringJUnit4ClassRunner.class)
+public abstract class AbstractServiceTest {
 }
