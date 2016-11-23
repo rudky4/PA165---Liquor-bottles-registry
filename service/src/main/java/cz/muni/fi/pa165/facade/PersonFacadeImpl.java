014e74a191a0ff59cced962fba2ce9ef438af070
Jakub Fiser <Fiser33@seznam.cz>
2016-11-23 14:37:31 +0100

    PersonService extended, PersonServiceImpl using password hash

--- service/src/main/java/cz/muni/fi/pa165/facade/PersonFacadeImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/facade/PersonFacadeImpl.java
@@ -1,5 +1,8 @@
 package cz.muni.fi.pa165.facade;
 
+import cz.muni.fi.pa165.entity.Laboratory;
+import cz.muni.fi.pa165.entity.Manufacturer;
+import cz.muni.fi.pa165.entity.Store;
 import cz.muni.fi.pa165.service.BeanMappingService;
 import cz.muni.fi.pa165.service.PersonService;
 import cz.muni.fi.pa165.dto.PersonDTO;
@@ -12,6 +15,8 @@ import javax.inject.Inject;
 import java.util.List;
 
 /**
+ * Implementation of PersonFacade interface.
+ *
  * @author Jakub Fiser
  * @date 14/11/2016
  */
@@ -33,12 +38,14 @@ public class PersonFacadeImpl implements PersonFacade {
 
     @Override
     public void changePassword(PersonDTO person, String newPassword) {
-
+        Person person1 = beanMappingService.mapTo(person, Person.class);
+        personService.changePassword(person1, newPassword);
     }
 
     @Override
     public boolean authenticate(PersonDTO person, String password) {
-        return false;
+        Person person1 = beanMappingService.mapTo(person, Person.class);
+        return personService.authenticate(person1, password);
     }
 
     @Override
@@ -47,11 +54,26 @@ public class PersonFacadeImpl implements PersonFacade {
         return personService.isPolice(police);
     }
 
-    @Override
-    public boolean isManufacturer(PersonDTO person) {
-        Person manufacturer = beanMappingService.mapTo(person, Person.class);
-        return personService.isManufacturer(manufacturer);
-    }
+//    @Override
+//    public boolean worksForLaboratory(PersonDTO person, LaboratoryDTO laboratory) {
+//        Person employee = beanMappingService.mapTo(person, Person.class);
+//        Laboratory employer = beanMappingService.mapTo(laboratory, Laboratory.class);
+//        return personService.worksForLaboratory(employee, employer);
+//    }
+//
+//    @Override
+//    public boolean worksForManufacturer(PersonDTO person, ManufacturerDTO manufacturer) {
+//        Person employee = beanMappingService.mapTo(person, Person.class);
+//        Manufacturer employer = beanMappingService.mapTo(manufacturer, Manufacturer.class);
+//        return personService.worksForManufacturer(employee, employer);
+//    }
+//
+//    @Override
+//    public boolean worksForStore(PersonDTO person, StoreDTO store) {
+//        Person employee = beanMappingService.mapTo(person, Person.class);
+//        Store employer = beanMappingService.mapTo(store, Store.class);
+//        return personService.worksForStore(employee, employer);
+//    }
 
     @Override
     public List<PersonDTO> findAll() {
@@ -67,6 +89,12 @@ public class PersonFacadeImpl implements PersonFacade {
     @Override
     public PersonDTO findUserByLogin(String login) {
         Person result = personService.findUserByLogin(login);
+        return result == null ? null : beanMappingService.mapTo(result, PersonDTO.class);
+    }
+
+    @Override
+    public PersonDTO findUserByEmail(String email) {
+        Person result = personService.findUserByEmail(email);
         return result == null ? null : beanMappingService.mapTo(result, PersonDTO.class);
     }
 }
